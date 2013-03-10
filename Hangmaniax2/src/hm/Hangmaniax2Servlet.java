be85d254cc22c91b417e28de250bf7f29cedd113
faddap <barambarero@gmail.com>
2013-03-10 19:41:18 +0200

    Layout and dummy login
    A basic layout and
    a few AJAX functions created.
    Servlet handles POST requests and
    sends 'echo'-like responses.

--- Hangmaniax2/src/hm/Hangmaniax2Servlet.java
+++ Hangmaniax2/src/hm/Hangmaniax2Servlet.java
@@ -1,14 +1,41 @@
 package hm;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.http.*;
+
+import com.google.appengine.labs.repackaged.org.json.JSONException;
+import com.google.appengine.labs.repackaged.org.json.JSONObject;
 
 @SuppressWarnings("serial")
 public class Hangmaniax2Servlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		resp.setContentType("text/plain");
-		resp.getWriter().println("Hello, world");
-		resp.getWriter().println("Nasko was here!");
+		resp.getWriter().write("hello");
+	}
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		String reqStr = req.getParameter("jsonRPC");
+		
+		PrintWriter wr = null;
+		try {
+			wr = resp.getWriter();
+			JSONObject jsonResp;
+			jsonResp = new JSONObject("{'error': 'Invalid input method or params'}");
+			
+			JSONObject jsonReq = new JSONObject(reqStr);
+			JSONObject jsonParams = jsonReq.has("params") ? jsonReq.optJSONObject("params") : null;
+			if (jsonReq.has("method") && jsonReq.optString("method").equals("login")) {
+				jsonResp = new JSONObject("{'success': true}");
+				jsonResp.put("credentials", jsonParams);
+			}
+			
+			wr.write(jsonResp.toString());
+		} catch(JSONException e) {
+			e.printStackTrace();
+		} finally {
+			wr.flush();
+			wr.close();
+		}
 	}
 }
--- Hangmaniax2/war/scripts/script.js
+++ Hangmaniax2/war/scripts/script.js
@@ -1,3 +1,33 @@
-$('document').ready(function(){
-	console.log('jQuery loaded...');
+$('document').ready(function() {
+	
+	//handle POST submit from signIn form
+	$('form#loginForm').bind({
+		'submit': function(e) {
+			e.preventDefault();
+			var cr = {username: this.username.value, password: this.password.value};
+			sendJsonRPC('login', cr, onLoginSucceeded);
+		}
+	});
+	
+	function sendJsonRPC(method, params, successCallback, errorCallback) {
+		var jsonObj = {method: method, params: params};
+		var onErr = (errorCallback != undefined && jQuery.isFunction(errorCallback)) ? errorCallback : onAjaxError
+		$.ajax({
+			url: '/hangmaniax2',
+			method: 'POST',
+			dataType: 'json',
+			data: {"jsonRPC": JSON.stringify(jsonObj)},
+			success: successCallback,
+			error: onErr
+		});
+	};
+	
+	function onLoginSucceeded(data) {
+		console.log(data);
+	};
+	
+	function onAjaxError(data) {
+		console.log('AJAX error:');
+		console.log(data);
+	};
 });
