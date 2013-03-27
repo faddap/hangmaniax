0d465f5a8d15708ad3050b023607a98cf0e0c5d7
faddap <barambarero@gmail.com>
2013-03-27 17:35:58 +0200

    Login and Logout completed. Session check. JsonRPC mask. Currently an ajax call is made upon a page load to verify if there is a valid session. Mask added for all JsonRPC AJAX requests.

--- Hangmaniax2/war/scripts/script.js
+++ Hangmaniax2/war/scripts/script.js
@@ -1,10 +1,36 @@
 $('document').ready(function() {
 	$signupDialog = $('div#signupDialog');
+	$loginForm = $('form#loginForm');
+	$userDetails = $('div#userDetails');
+	
+	// Check for browser session.
+	// If not needed a simple refresh would serve as logout instead.
+	sendJsonRPC('checkSession', {}, function(resp) {
+		if (responseIsSuccessful(resp)) {
+			onLoginSucceeded(resp);
+		} else {
+			
+		}
+	});
+	
 	$signupDialog.dialog({
 		title: 'SignUp!',
 		modal: true,
 		autoOpen: false,
 		draggable: false
+	});
+	
+	$errorDialog = $("<div />").css({'text-align': 'center'}).dialog({
+		title: 'Error!',
+		modal: true,
+		autoOpen: false,
+		draggable: false,
+		buttons: [{
+			text: 'Ok',
+			click: function() {
+				$(this).dialog('close');
+			}
+		}]
 	});
 	
 	//handle POST submit from signIn form
@@ -25,6 +51,18 @@ $('document').ready(function() {
 		}
 	});
 	
+	//handle logout
+	$userDetails.children('a#logoutBtn').bind({
+		'click': function(e) {
+			e.preventDefault();
+			sendJsonRPC('logout', {}, onLogoutSucceeded);
+		}
+	});
+	
+	function responseIsSuccessful(resp) {
+		return resp.result != undefined && resp.result.success != undefined && resp.result.success === true;
+	};
+	
 	function sendJsonRPC(method, params, successCallback, errorCallback) {
 		var jsonObj = {method: method, params: params};
 		var onErr = (errorCallback != undefined && jQuery.isFunction(errorCallback)) ? errorCallback : onAjaxError
@@ -34,22 +72,64 @@ $('document').ready(function() {
 			dataType: 'json',
 			data: {"jsonRPC": JSON.stringify(jsonObj)},
 			success: successCallback,
-			error: onErr
+			error: onErr,
+			
+			beforeSend: function() {
+				mask();
+			},
+			complete: function() {
+				mask(false);
+			}
 		});
 	};
 	
-	function onLoginSucceeded(data) {
-		console.log(data);
+	function onLoginSucceeded(resp) {
+		if (responseIsSuccessful(resp)) {
+			var params = resp.result;
+			$loginForm.hide();
+			var usernameParam = $userDetails.children('span.username').text();
+			var scoreParam = $userDetails.children('span.score').text();
+			$userDetails.children('span.username').text(usernameParam.replace('{username}', params.name));
+			$userDetails.children('span.score').text(scoreParam.replace('{score}', params.score));
+			$userDetails.show();
+		} else {
+			showError("Wrong email and/or password!");
+		}
 	};
 	
-	function onSignupSucceeded(data) {
+	function onSignupSucceeded(resp) {
 		$signupDialog.dialog('close');
-		console.log(data);
+		console.log(resp);
+	};
+	
+	function onLogoutSucceeded(resp) {
+		if (responseIsSuccessful(resp)) {
+			$userDetails.hide();
+			$loginForm.show();
+		}
 	};
 	
 	function onAjaxError(data) {
 		console.log('AJAX error:');
 		console.log(data);
+	};
+	
+	function showError(errMsg) {
+		$errorDialog.text(errMsg).dialog('open');
+	};
+	
+	function mask(openMask) {
+		if (openMask == undefined || openMask == true) {
+			$('<div id="mask" />')
+				.css({
+					width: '100%',
+					height: '100%'
+				})
+				.attr('class', 'ui-widget-overlay ui-front')
+				.appendTo($('body'));
+		} else {
+			$('div#mask').remove();
+		}
 	};
 	
 	//handle signUp menu click
